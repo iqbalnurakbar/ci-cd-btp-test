50f4b29f81b6d23cf785f8b094d852086a5750bf
Iqbal <iqbal.nurakbar25@gmail.com>
2025-09-16 09:41:37 +0700

    add hello button

--- btpdeploy/webapp/controller/View1.controller.js
+++ btpdeploy/webapp/controller/View1.controller.js
@@ -1,10 +1,13 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  (Controller, MessageToast) => {
     "use strict";
 
     return Controller.extend("btpdeploy.controller.View1", {
-        onInit() {
-        }
+      onInit() {},
+      sayHello: function () {
+        MessageToast.show("Hello!");
+      },
     });
-});
+  }
+);
